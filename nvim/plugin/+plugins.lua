1a98fb38045fb24b19e7cb52e30cf5fdbee8ac6e
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-08-25 15:12:22 +0200

    update for lazy cmake loading

--- nvim/plugin/+plugins.lua
+++ nvim/plugin/+plugins.lua
@@ -1,8 +1,8 @@
 vim.pack.add({
     -- deps
-    { src = "https://github.com/nvim-tree/nvim-web-devicons.git" }, -- statusline, neo-tree, bufferline
-    { src = "https://github.com/nvim-lua/plenary.nvim.git" }, -- neo-tree, cmake-tools
-    { src = "https://github.com/MunifTanjim/nui.nvim.git" }, -- neo-tree
+    { src = "https://github.com/nvim-tree/nvim-web-devicons.git" }, -- needed by: statusline, neo-tree, bufferline
+    { src = "https://github.com/nvim-lua/plenary.nvim.git" }, -- needed by: neo-tree, cmake-tools
+    { src = "https://github.com/MunifTanjim/nui.nvim.git" }, -- needed by: neo-tree
 
     -- lsp
     { src = "https://github.com/mason-org/mason.nvim" },
@@ -13,14 +13,15 @@ vim.pack.add({
     { src = "https://github.com/nvim-lualine/lualine.nvim.git" },
     -- buffer tabs
     { src = "https://github.com/akinsho/bufferline.nvim.git" },
-    { src = "https://github.com/famiu/bufdelete.nvim.git" },
     -- commenting
     { src = "https://github.com/tpope/vim-commentary.git" },
     -- inline terminal
-    { src = "https://github.com/akinsho/toggleterm.nvim.git" }, -- cmake-tools
-    -- cmake
-    { src = "https://github.com/Civitasv/cmake-tools.nvim.git" },
-}, { load = true })
+    { src = "https://github.com/akinsho/toggleterm.nvim.git" }, -- needed by: cmake-tools
+}, { confirm = false, load = false })
+vim.pack.add({
+    -- deps
+    { src = "https://github.com/famiu/bufdelete.nvim.git" }, -- needed by: bufferline (indirectly)
+}, { confirm = false, load = true })
 
 -- lsp
 require("blink.cmp").setup({
@@ -41,9 +42,10 @@ require("blink.cmp").setup({
 })
 
 -- file browser
-vim.keymap.set("n", "<C-e>", "<Cmd>Neotree toggle<CR>")
-require("neo-tree").setup({
-})
+require("neo-tree.command").execute({ toggle = true }) -- should open with neo-tree visible
+vim.keymap.set("n", "<C-e>", function()
+    require("neo-tree.command").execute({ toggle = true })
+end)
 
 -- statusline
 require("lualine").setup({
@@ -102,23 +104,36 @@ require("toggleterm").setup({
     open_mapping = "<C-j>"
 })
 
-require("cmake-tools").setup({
-    cmake_build_directory = "build",
-    working_directory = "/home/jan/repos/chad_vis",
-    cmake_compile_commands_options = {
-        action = "soft_link",
-    },
-    cmake_executor = {
-        name = "toggleterm",
-        opts = {
-            direction = "horizontal",
-        },
-    },
-    cmake_runner = {
-        name = "toggleterm",
-        opts = {
-            direction = "horizontal",
-        },
-    },
-
+-- cmake
+vim.api.nvim_create_autocmd({ "DirChanged" }, {
+    callback = function()
+        -- check if directory contains a CMakeLists.txt
+        local cwd = vim.loop.cwd()
+        if vim.fn.filereadable(cwd .. "/CMakeLists.txt") then
+            -- immediately load with vim.pack
+            vim.pack.add({
+                "https://github.com/Civitasv/cmake-tools.nvim.git"
+            }, { confirm = false, load = true })
+            -- set up paths (generate options and build dir dont work)
+            require("cmake-tools").setup({
+                cmake_use_preset = false,
+                cmake_generate_options = { "-G Ninja" },
+                cmake_build_directory = "build",
+                cmake_executor = {
+                    name = "toggleterm",
+                    opts = {
+                        direction = "horizontal",
+                    },
+                },
+                cmake_runner = {
+                    name = "toggleterm",
+                    opts = {
+                        direction = "horizontal",
+                    },
+                },
+            })
+            -- since build dir opt doesnt work, set it manually
+            vim.cmd("CMakeSelectBuildDir " .. cwd .. "/build")
+        end
+    end,
 })
